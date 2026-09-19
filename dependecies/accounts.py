8480d77ec6882838e1227ce305c64909e3a38725
KirillovD <92786283+KirillovD@users.noreply.github.com>
2026-09-19 23:48:30 +0200

    fix: validate credit accounts with AccountType enum

--- dependecies/accounts.py
+++ dependecies/accounts.py
@@ -6,29 +6,30 @@ import models
 from crud import accounts
 from database import get_db
 from dependecies.users import get_current_user
+from enums import AccountType
 
 
-def get_valid_acc(acc_id : int,
-                  user : models.User = Depends(get_current_user),
-                  db : Session = Depends(get_db)):
-
-    account = accounts.get_acc_by_id(acc_id,db)
+def get_valid_acc(
+    acc_id: int,
+    user: models.User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    account = accounts.get_acc_by_id(acc_id, db)
 
     if not account:
         raise exceptions.AccountNotFound()
 
-    if not account.owner_id == user.id:
+    if account.owner_id != user.id:
         raise exceptions.NotYourAccount()
 
     return account
-
 
 
 def get_valid_credit_acc(
     acc_id: int,
     account: models.Account = Depends(get_valid_acc),
 ):
-    if account.type != models.Account.type.property.columns[0].type.enum_class.CREDIT:
+    if account.type != AccountType.CREDIT:
         raise exceptions.AccountOperationNotAllowed(
             detail="This endpoint requires a credit account"
         )
